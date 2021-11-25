c012b1da7b079ba813505ca46d603346ece1e864
Nuha <saifinuha1001@gmail.com>
2021-11-25 17:54:02 +0700

    commit pertama

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title : 'Nature',
+      imageUrl : 'assets/tree.jpeg',
+      username : 'nature',
+      content : 'Saw this awesome tree during my hike today'
+    },
+    {
+      title : 'Snowy Mountain',
+      imageUrl : 'assets/mountain.jpeg',
+      username : 'hikingperson',
+      content : 'Beautiful view of some mountains I saw during my hike'
+    },
+    {
+      title : 'Mountain Biking',
+      imageUrl : 'assets/biking.jpeg',
+      username : 'biking12222',
+      content : 'Action shot of me riding my bike'
+    },
+  ]
 }
